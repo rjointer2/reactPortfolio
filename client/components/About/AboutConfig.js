d84107233e99bf408ea02b3acb381d74f0d9d565
Roody <rjointer2@gmail.com>
2021-06-19 20:54:18 -0700

    hello world, saving the progress

--- client/components/About/AboutConfig.js
+++ client/components/About/AboutConfig.js
@@ -9,7 +9,7 @@ export const aboutConfig = {
     alt: 'Roody',
     title: `Junior Web Developer`,
     heading: `Roosevelt Jointer`,
-    text: `
+    text:`
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
